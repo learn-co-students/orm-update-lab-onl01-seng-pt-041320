4b12556c77eacc90d0f0f3cffb7ce34f049d6e60
marcusLau <lau.marcus76@gmail.com>
2020-06-17 14:26:56 -0700

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -15,12 +15,77 @@ class Student
 
   def self.create_table
     sql = <<-SQL
-      CREATE TABLE IF NOT EXISTS students
-      id INTEGER PRIMARY KEY,
-      name TEXT,
-      grade TEXT
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT
+      )
       SQL
       DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE IF EXISTS students
+      SQL
+      DB[:conn].execute(sql)
+  end
+
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO students (name, grade)
+        VALUES (?, ?)
+        SQL
+      DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
+  end
+
+  def self.create(name, grade)
+    student = self.new(name, grade)
+    student.save
+    student
+  end
+
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    grade = row[2]
+    student = self.new(id, name, grade)
+    student
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+    SELECT * 
+    FROM students
+    WHERE name = ?
+    SQL
+    DB[:conn].execute(sql, name).map do |row|
+      new_from_db(row)
+    end.first
+    # dont have to use self.new_from_row since already in self.method scope
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  def update 
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
 
 end
